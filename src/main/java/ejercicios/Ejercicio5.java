e938011c84a2c5c3073ae2195c14b85174f136db
carlos.iglesiasgomez <carlos.iglesiasgomez@WIRTZ.LOCAL>
2021-03-01 19:41:19 +0100

    5

---
+++ src/main/java/ejercicios/Ejercicio5.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+/**
+ * A partir de la clase Ahorcado que te proporcionará el profesor, crea un
+ * programa que permita al usuario jugar al ahorcado. Desconocemos los atributos
+ * de la clase Ahorcado ya que son privados, pero sus métodos públicos son:
+ */
+
+public class Ejercicio5 {
+
+    
+}
