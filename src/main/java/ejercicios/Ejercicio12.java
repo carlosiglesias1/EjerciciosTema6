96aae5c41b80f866d93bf2967dfc0bc8c8c5d680
carlosiglesias1 <carlosig490@gmail.com>
2021-03-14 03:08:27 +0100

    inicializacion de prueba del ejercicio 12

---
+++ src/main/java/ejercicios/Ejercicio12.java
@@ -0,0 +1,18 @@
+package ejercicios;
+
+/**
+ * 6.12. Diseña una clase llamada Alumno para gestionar los alumnos de una
+ * escuela. Contiene los atributos: nombre completo, DNI, fecha de nacimiento y
+ * nombre de la escuela (común para todos los alumnos). Además del constructor,
+ * los métodos set y get para cada atributo, tendrá los siguientes métodos:
+ * -Método que nos dice si es mayor de edad o no. - Método al que se le pase
+ * como parámetro otro alumno y nos devuelva true si el alumno pasado es menor
+ * que él mismo. False en caso contrario. - Método al que se le pase como
+ * parámetro otro alumno y nos devuelva true si el alumno pasado es exactamente
+ * igual en todos los campos (un duplicado). False en caso contrario. Realiza a
+ * continuación un programa sencillo que use la clase definida.
+ */
+
+public class Ejercicio12 {
+    
+}
